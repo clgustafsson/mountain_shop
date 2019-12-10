fabbd463088609dae6f308dcf18a9074eccd16ad
clgustafsson <58473037+clgustafsson@users.noreply.github.com>
2019-12-10 18:14:10 +0100

    update

--- js/javascript.js
+++ js/javascript.js
@@ -2,13 +2,14 @@ function gotoproduct() {
     window.open("index2.html", "_self");
 }
 
-function showpopup(x) {
-    document.getElementById("popupviewproduct").style.display = "grid";
-    var test = document.getElementsByClassName("productinfo");
-    //test[0].style.display = "none"
-}
+//function showpopup(x) {
+//    document.getElementById("popupviewproduct").style.width = x.style.width;
+//    document.getElementById("popupviewproduct").style.display = "grid";
+//    var test = document.getElementsByClassName("productinfo");
+//    //test[0].style.display = "none"
+//}//
 
-function hidepopup(x) {
-    document.getElementById("popupviewproduct").style.display = "none";
-    x.style.width = "100%";
-}
+//function hidepopup(x) {
+//    document.getElementById("popupviewproduct").style.display = "none";
+//    x.style.width = "100%";
+//}

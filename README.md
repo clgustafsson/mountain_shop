685849f0d12f17a6adf02b82e88cf5da7c6a60a4
clgustafsson <58473037+clgustafsson@users.noreply.github.com>
2019-12-10 13:31:45 +0100

    update

--- README.md
+++
@@ -1,4 +0,0 @@
-# topsecret
-This is an anonymous website for people who wants to buy mountains
-
-Only costs your and your childrens kidneys!
--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,14 @@
 function gotoproduct() {
-    window.open("index2.html","_self");
+    window.open("index2.html", "_self");
 }
+
 function showpopup(x) {
-    document.getElementById("popupviewproduct").style.display = "grid"; 
-    x.style.width = "200%";
-    x.style.zIndex = "6";
+    document.getElementById("popupviewproduct").style.display = "grid";
+    var test = document.getElementsByClassName("productinfo");
+    //test[0].style.display = "none"
 }
+
 function hidepopup(x) {
-    document.getElementById("popupviewproduct").style.display = "none"; 
+    document.getElementById("popupviewproduct").style.display = "none";
     x.style.width = "100%";
 }

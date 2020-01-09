5ab44cc68ebcab9524bdd324407f3d7646b25204
clgustafsson <58473037+clgustafsson@users.noreply.github.com>
2020-01-09 11:35:59 +0100

    update

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,48 @@
+//window.onload = initDivMouseOver;
+var mouse = false;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function mouseover(x) {
+    mouse = x;
+}
+
+async function popup(elem) {
+
+    const buyItem = document.getElementById("popupviewproduct");
+    elem.style.transform = "scale(1.027, 1.027)"
+    //elem.style.width = elem.clientWidth.toString() + "px";
+    buyItem.style.position = "absolute";
+    buyItem.style.display = "grid";
+    elem.append(buyItem);
+    buyItem.style.bottom = "-" + buyItem.clientHeight.toString() + "px";
+}
+
+async function hidepopup(elem) {
+
+    if (mouse == false) {
+        const buyItem = document.getElementById("popupviewproduct");
+        elem.style.width = "100%";
+        buyItem.style.display = "none";
+        buyItem.style.position = "static";
+        elem.style.transform = "initial";
+    }
+}
+
+//async function hidepopup(elem) {
+//    elem.style.transform = "initial";
+//    await sleep(10);
+//    if (mouse == false) {
+//        const buyItem = document.getElementById("popupviewproduct");
+//        parentDiv = document.getElementById("home");
+//        elem.style.width = "100%";
+//        buyItem.style.display = "none";
+//        parentDiv.appendChild(buyItem);
+//    }
+//}
+
 function gotoproduct() {
     window.open("index2.html", "_self");
 }
-
-//function showpopup(x) {
-//    document.getElementById("popupviewproduct").style.width = x.style.width;
-//    document.getElementById("popupviewproduct").style.display = "grid";
-//    var test = document.getElementsByClassName("productinfo");
-//    //test[0].style.display = "none"
-//}//
-
-//function hidepopup(x) {
-//    document.getElementById("popupviewproduct").style.display = "none";
-//    x.style.width = "100%";
-//}

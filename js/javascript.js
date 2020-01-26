59da9e57ac42bca652489766c6372193a9f19752
clgustafsson <58473037+clgustafsson@users.noreply.github.com>
2020-01-26 14:18:32 +0100

    update

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@ var myBag = [];
 
 var items = [{ //name: has to be unique. /product image -> css/"name:".webp
         price: 20,
-        name: "Product1",
+        name: "product1",
         sale: false,
         oldprice: 0,
         saletag: ""
